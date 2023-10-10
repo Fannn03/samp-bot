6e8619bfcd414fcd2f29dc28a78115f3906eba27
Fannn03 <fann.fann771@gmail.com>
2023-10-10 23:19:29 +0700

    feat: add interaction modal submit

--- events/interaction-create.js
+++ events/interaction-create.js
@@ -1,10 +1,15 @@
-import { ButtonInteraction } from "discord.js"
+import { ButtonInteraction, ModalSubmitInteraction } from "discord.js"
 import { registerUcpModal } from "../helper/modal/register-ucp.js"
+import { registerUcpForm } from "../helper/form/register-ucp.js";
 
 export default async (interaction) => {
-  if(interaction instanceof ButtonInteraction) {
-    const { customId } = interaction
+  const { customId } = interaction
 
+  if(interaction instanceof ButtonInteraction) {
     if(customId === "register-ucp") return registerUcpModal(interaction)
+  }
+  
+  if(interaction instanceof ModalSubmitInteraction) {
+    if(customId === "register-ucp-modal") return registerUcpForm(interaction)
   }
 }
