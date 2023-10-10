a6ac87bbf51841c9b07863f080d8b902589f2adb
Fannn03 <fann.fann771@gmail.com>
2023-10-10 23:19:02 +0700

    feat: register ucp middleware using joi

---
+++ middleware/register-ucp-middleware.js
@@ -0,0 +1,24 @@
+import Joi from "joi"
+
+export default async (request) => {
+
+  const form = Joi.object({
+    username: Joi.string()
+      .pattern(/^\S+$/)
+      .messages({
+        "string.pattern.base": "Username tidak boleh mengandung spasi"
+      })
+    ,
+    password: Joi.string()
+      .pattern(/^\S+$/)
+      .messages({
+        "string.pattern.base": "Password tidak boleh mengandung spasi"
+      })
+  })
+
+  try {
+    await form.validateAsync(request)
+  } catch (err) {
+    throw new Error(err.details[0].message)
+  }
+}
