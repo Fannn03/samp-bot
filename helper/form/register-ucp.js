d28c79e99517a518202eae78807b0eca38a38273
Fannn03 <fann.fann771@gmail.com>
2023-10-11 13:03:46 +0700

    feat: register ucp message

--- helper/form/register-ucp.js
+++ helper/form/register-ucp.js
@@ -1,6 +1,8 @@
 import { EmbedBuilder } from "discord.js"
+import 'dotenv/config'
 import registerUcpMiddleware from "../../middleware/register-ucp-middleware.js"
 import { registerUcp } from "../../repositories/ucp.js"
+import { registerUcpDirectMessage } from "../messages/register-ucp.js"
 
 export const registerUcpForm = async (interaction) => {
   const username = interaction.fields.getTextInputValue('username')
@@ -11,10 +13,40 @@ export const registerUcpForm = async (interaction) => {
     username: username,
     password: password
   }
+
   try {
     await registerUcpMiddleware(request)
     await registerUcp(request)
+
+    await interaction.user.send({
+      embeds: [
+        new EmbedBuilder()
+          .setColor("Green")
+          .setThumbnail(interaction.guild.iconURL())
+          .setTitle(`**Berhasil terdaftar di ${interaction.guild.name}**`)
+          .setDescription(registerUcpDirectMessage(request, process.env.CHANNEL_REGISTER, process.env.CHANNEL_SUPPORT))
+          .setTimestamp()
+      ]
+    })
+
+    await interaction.reply({
+      embeds: [
+        new EmbedBuilder()
+          .setColor("Green")
+          .setTitle("**Registrasi berhasil**")
+          .setDescription("Silahkan periksa DM mu.")
+      ],
+      ephemeral: true
+    })
+
+    return setTimeout(async () => {
+      await interaction.deleteReply()
+    }, 10000)
   } catch (err) {
+    if(err.name === "SequelizeUniqueConstraintError") {
+      err.message = err.errors[0].message
+    }
+
     const message = await interaction.reply({
       embeds: [
         new EmbedBuilder()
--- helper/messages/register-ucp.js
+++ helper/messages/register-ucp.js
@@ -1,7 +1,19 @@
-export const registerUcpMessage = (channel) => {
+export const registerUcpCommandMessage = (channel) => {
   return `
   Dengan menekan tombol dibawah ini, kamu menyetujui peraturan yang ada di server ini.
 
   Jika memiliki pertanyaan atau kendala kamu bisa menghubungi ke <#${channel}>
+  `
+}
+
+export const registerUcpDirectMessage = (request, register, support) => {
+  return `
+  > **Username :** ${request.username}
+  > **Password :** ||${request.password}||
+
+  Silahkan buat karakter mu terlebih dahulu di <#${register}>
+  Jika kamu memiliki kendala atau pertanyaan, silahkan tanyakan kepada staff server dengan membuat ticket di <#${support}>
+
+  Segala bentuk data yang tidak benar akan kami ubah sewaktu waktu tanpa sepengetahuan dari kamu.
   `
 }
